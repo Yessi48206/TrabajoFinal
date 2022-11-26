23112039e6c856ffb8f8b067db45fde66f1a700b
unknown <dayleiva92@gmail.com>
2022-11-25 22:17:21 -0300

    arreglamos el link de imagen de sobre nosostros

--- src/components/views/CompVarios/CardInicio.jsx
+++ src/components/views/CompVarios/CardInicio.jsx
@@ -3,15 +3,12 @@ import pintas from '../img/pintas.jpg';
 import ginTonic from '../img/ginTonic.jpg';
 import pizza2 from '../img/pizza2.jpg';
 import dosGin from '../img/dosGin.jpg';
-import { Button } from 'react-bootstrap';
 
 const CardInicio = () => {
     return (
         <div className='row my-5 py-5 sau'>
             <div className='col-3 my-2'>
                 <img src={pintas} alt="" className='w-100 imgProduc '/>
-                <Button>Ver Mas</Button>
-
 
             </div>
             <div className='col-3 my-2'>
